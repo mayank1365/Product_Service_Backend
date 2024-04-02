77c0397b2233c8b7b217722195408c91918745d0
Mayank <mayank1365@gmail.com>
2024-04-02 17:08:02 +0530

    Initial commit

---
+++ src/main/java/com/mayank/product_service_sst/ProductServiceSstApplication.java
@@ -0,0 +1,13 @@
+package com.mayank.product_service_sst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceSstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceSstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mayank/product_service_sst/ProductServiceSstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayank.product_service_sst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceSstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
