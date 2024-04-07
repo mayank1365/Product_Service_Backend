9c69666d336b7a6cf7c6824cc67a3d78b645607a
Mayank <mayank1365@gmail.com>
2024-04-07 22:22:31 +0530

    Exceptions handling

---
+++ src/main/java/com/mayank/product_service_sst/dtos/ExceptionDto.java
@@ -0,0 +1,12 @@
+package com.mayank.product_service_sst.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ExceptionDto {
+    private String message;
+    private String resolution;
+}
+
